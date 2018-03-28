f0599f801d410ac2315c3a0fd9fac3d8a0b38b74
Alex Braz <alexjavabraz@gmail.com>
2018-03-28 11:51:45 -0300

    adding server methods

--- server/methods.js
+++ server/methods.js
@@ -1,9 +1,19 @@
 Meteor.methods({
     adiciona : function(obj){
+
+        if (! this.userId) {
+            throw new Meteor.Error('not-authorized');
+          }
+
         Tarefas.insert({nome:obj.nome, data: new Date(), usuario: this.userId});
     },
     remove : function(id){
-        Tarefas.remove({_id: id});
+
+        if (! this.userId) {
+            throw new Meteor.Error('not-authorized');
+          }
+
+        Tarefas.remove({_id: id, usuario: this.userId});
     }
 
 });
